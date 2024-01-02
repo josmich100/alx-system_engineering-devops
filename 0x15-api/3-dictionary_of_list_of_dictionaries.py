290805fbfdf2c14549bd53171936abbb7d9f330a
Josiah Michina <61215484+josmich100@users.noreply.github.com>
2024-01-02 23:10:55 +0300

    update 5

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,47 +1,21 @@
 #!/usr/bin/python3
 
-"""
-This module retrieves TODO list progress for all employees from a REST API and
-exports it in JSON format
-"""
+"""Export data in the JSON format"""
 
 import json
 import requests
-import sys
-
-
-def get_employee_todo_list_progress():
-    """
-    Retrieve TODO list progress for all employees from a REST API
-    """
-    url = 'https://jsonplaceholder.typicode.com/todos'
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        todos = response.json()
-
-        todo_dict = {}
-        for todo in todos:
-            employee_id = todo['userId']
-            employee_name = todo['username']
-            task_title = todo['title']
-            task_completed = todo['completed']
-
-            if employee_id in todo_dict:
-                todo_dict[employee_id].append(
-                    {"username": employee_name, "task": task_title, "completed": task_completed})
-            else:
-                todo_dict[employee_id] = [
-                    {"username": employee_name, "task": task_title, "completed": task_completed}]
-
-        with open('todo_all_employees.json', mode='w') as json_file:
-            json.dump(todo_dict, json_file)
-
-        print("Exported TODO list progress for all employees to todo_all_employees.json")
-
-    else:
-        print("Error retrieving TODO list progress for all employees")
+from sys import argv
 
 
 if __name__ == "__main__":
-    get_employee_todo_list_progress()
+    url = 'https://jsonplaceholder.typicode.com/'
+    users = requests.get(url + 'users').json()
+    todo = requests.get(url + 'todos').json()
+
+    with open('todo_all_employees.json', 'w') as jsonfile:
+        json.dump({user.get('id'): [{
+            "task": task.get('title'),
+            "completed": task.get('completed'),
+            "username": user.get('username')
+        } for task in todo if user.get('id') == task.get('userId')]
+            for user in users}, jsonfile)
