c66a108f0fed8d53214a28eb4191d7aebfd8a6c8
Josiah Michina <61215484+josmich100@users.noreply.github.com>
2024-01-02 22:52:31 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-This module retrieves TODO list progress for a given employee ID from a REST API
+This module retrieves TODO list progress for a given employee ID from a REST API.
 """
 
 import requests
@@ -10,8 +10,10 @@ import sys
 
 def get_employee_todo_list_progress(employee_id):
     """
-    Retrieve TODO list progress for a given employee ID from a REST API:
-    param employee_id: integer representing the ID of the employee whose progress is to be retrieved
+    Retrieve TODO list progress for a given employee ID from a REST API.
+
+    Args:
+        employee_id (int): The ID of the employee whose progress is to be retrieved.
     """
     url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
     response = requests.get(url)
@@ -23,15 +25,13 @@ def get_employee_todo_list_progress(employee_id):
         num_completed_tasks = len(completed_tasks)
 
         employee_name = todos[0]['username']
-        print(
-            f"Employee {employee_name} is done with tasks({num_completed_tasks}/{total_tasks}):")
+        print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{total_tasks}):")
 
         for task in completed_tasks:
-            print("\t {} {}".format(task['title'], '\n'))
+            print(f"\t{task['title']}")
 
     else:
-        print(
-            f"Error retrieving TODO list progress for employee ID {employee_id}")
+        print(f"Error retrieving TODO list progress for employee ID {employee_id}")
 
 
 if __name__ == "__main__":
