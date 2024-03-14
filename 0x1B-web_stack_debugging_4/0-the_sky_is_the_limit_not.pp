4cf9f77d3e9a24333b555ee679c929580e9b65dd
josmich100 <michinajosiah@gmail.com>
2024-03-15 02:14:56 +0300

    free

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,23 +1,6 @@
 # File: 0-the_sky_is_the_limit.pp
 
-# Define an exec resource to run the ab command
-exec { 'ab_benchmark':
-  command     => '/usr/bin/ab -c 100 -n 2000 http://localhost/',
-  logoutput   => true,
-  refreshonly => true,
+exec {'modify max open files limit setting':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
-
-# Define the Nginx service
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => Exec['ab_benchmark'],
-}
-
-# Define a file resource to manage the Nginx default configuration
-file { '/etc/nginx/sites-available/default':
-  ensure  => 'file',
-  content => template('path/to/your/nginx_config.erb'), # You need to provide the correct path to your Nginx config template
-  notify  => Service['nginx'],
-}
-
